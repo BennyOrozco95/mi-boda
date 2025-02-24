7c2ea471a0573120fad7c3b8e4d12e2083fef5bf
BennyOrozco95 <75332165+BennyOrozco95@users.noreply.github.com>
2025-02-24 05:44:07 +0000

    fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -14,4 +14,4 @@ export default {
     },
   },
   plugins: [],
-};
+}
